dc9271e135d74faa003d6a2d8036c68fc361aa22
李青阳 <3131492575@qq.com>
2024-11-10 15:04:11 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -13,7 +13,7 @@ export default defineUserConfig({
   description: 'Test',
   bundler: viteBundler(),
   head: [
-    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/Velvet-Blog/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
   ],
   alias: {
     '@theme/VPBackToTop.vue': path.resolve(
